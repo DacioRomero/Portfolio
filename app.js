ebdf85debf6f1c883fdf24c3e84e112f8b7f46e2
Dacio Romero <dacioromero@gmail.com>
2018-09-06 10:51:42 -0700

    Revert "Test subdomain functionality"
    This reverts commit d0a726c458fd7f95d9700202fe725b4cbae1ac56.

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 // app.js
 
-const subdomain = require('express-subdomain');
-const express = require('express');
-
-const app = express();
-const router = express.Router()
-
+const app = require('express')();
 app.engine('handlebars', require('express-handlebars')({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-if (process.env.SUBDOMAIN) {
-    console.log(`Hosting on subdomain ${process.env.SUBDOMAIN}`)
-    app.use(subdomain(process.env.SUBDOMAIN, router));
-}
-else {
-    app.use(router);
-}
-
 if (require.main === module) {
     let port = process.env.PORT || 3000;
-
+    
     app.listen(port, () => {
         console.log(`App listening on port ${port}!`);
     });
