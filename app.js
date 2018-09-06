1bd46b97d8b07621b9cb707ea48bb16dbfa2da2b
Dacio Romero <dacioromero@gmail.com>
2018-09-06 10:51:42 -0700

    Add blog, boostrap nav and footer

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 // app.js
 
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const marked  = require('marked');
+const express        = require('express');
+const exphbs         = require('express-handlebars');
+const methodOverride = require('method-override')
+const mongoose       = require('mongoose');
+const bodyParser     = require('body-parser');
+const marked         = require('marked');
 
 const app = express();
+
+const PostsController = require('./controllers/posts')
+
+// Middleware
+// hbs = exphbs.create({
+//     defaultLayout: 'main',
+//     extname: '.hbs',
+//     helpers: {
+//         mdToHTML: function (md) { marked(md); },
+//         shorten: function (s, n) { return s.slice(n)  }
+//     }
+// });
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
-    helpers: { marked: marked }
+    helpers: {
+        mdToHTML: marked,
+        shorten: (s, n) => { return s.split('\n').slice(0, n).join('\n'); },
+        ifOut: (x, y, a, b) => { return x == y ? a : b }
+    }
 }));
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dacio-app', { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index', { active: 'home' });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { active: 'about' });
 });
 
-app.get('/test', (req, res) => {
-    res.render('page', { content: '*test*' } );
-});
+PostsController(app)
+
+// app.get('/test', (req, res) => {
+//     res.render('post', { content: '*test*' } );
+// });
 
 if (require.main === module) {
     let port = process.env.PORT || 3000;
---
+++ controllers/posts.js
@@ -0,0 +1,72 @@
+// controllers/posts.js
+
+const Post = require('../models/post')
+
+function PostsController(app) {
+
+    app.get('/posts', (req, res) => {
+        Post.find()
+            .then(posts => {
+                res.render('posts-index', { posts: posts, active: 'blog' });
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
+
+    app.get('/posts/new', (req, res) => {
+      res.render('posts-new');
+    });
+
+    // CREATE
+    app.post('/posts', (req, res) => {
+        Post.create(req.body).then((post) => {
+            res.redirect(`/posts/${post._id}`);
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    });
+
+    // SHOW
+    app.get('/posts/:id', (req, res) => {
+        Post.findById(req.params.id).then(post => {
+            res.render('posts-show', { post: post, active: 'blog' });
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    });
+
+    // EDIT
+    app.get('/posts/:id/edit', (req, res) => {
+        Post.findById(req.params.id).then(post => {
+            res.render('posts-edit', { post: post, active: 'blog' });
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    });
+
+    // UPDATE
+    app.put('/posts/:id', (req, res) => {
+        Post.findByIdAndUpdate(req.params.id, req.body)
+            .then(review => {
+                res.redirect(`/posts/${review._id}`);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
+
+    // DELETE
+    app.delete('/posts/:id', (req, res) => {
+        Post.findByIdAndRemove(req.params.id)
+            .then(review => {
+                res.redirect('/posts');
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    });
+
+}
+
+module.exports = PostsController;
