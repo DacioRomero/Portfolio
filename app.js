650f88d7511c4702ee11c17e4d868196d4190449
Dacio Romero <dacioromero@gmail.com>
2018-09-06 13:03:12 -0700

    Add support for posts formatted with markdown

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 // app.js
 
-const app = require('express')();
-app.engine('handlebars', require('express-handlebars')({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const marked  = require('marked');
+
+const app = express();
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: { marked: marked }
+}));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/test', (req, res) => {
+    res.render('page', { content: '*test*' } );
+});
+
 if (require.main === module) {
     let port = process.env.PORT || 3000;
-    
+
     app.listen(port, () => {
         console.log(`App listening on port ${port}!`);
     });
