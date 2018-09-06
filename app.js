d0a726c458fd7f95d9700202fe725b4cbae1ac56
Dacio Romero <dacioromero@gmail.com>
2018-09-06 10:38:12 -0700

    Test subdomain functionality

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 // app.js
 
-const app = require('express')();
+const subdomain = require('express-subdomain');
+const express = require('express');
+
+const app = express();
+const router = express.Router()
+
 app.engine('handlebars', require('express-handlebars')({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('home');
 });
 
+if (process.env.SUBDOMAIN) {
+    console.log(`Hosting on subdomain ${process.env.SUBDOMAIN}`)
+    app.use(subdomain(process.env.SUBDOMAIN, router));
+}
+else {
+    app.use(router);
+}
+
 if (require.main === module) {
     let port = process.env.PORT || 3000;
-    
+
     app.listen(port, () => {
         console.log(`App listening on port ${port}!`);
     });
